0476e3c77557d3f72f45394ef593a807c914be4b
Marco Gonzalez <marcgonz@yahoo.com>
2022-02-16 08:59:09 -0600

    Semana 5 día 1

---
+++ Clases/Clase_S5_d1.R
@@ -0,0 +1,41 @@
+# MAGT
+# Semana 5 
+# 16/02/2022
+
+
+url <- paste0("https://raw.githubusercontent.com/mgtagle/",
+"PrincipiosEstadistica2021/main/cuadro1.csv")
+
+inventario <- read.csv(url)
+summary(inventario)
+
+inventario$Especie <- as.factor(inventario$Especie)
+summary(inventario)
+inventario$Posicion <- as.factor(inventario$Posicion)
+summary(inventario)
+
+# Obtener una tabla de frecuencia para las variables Especie
+# y Posición. Usar la función table
+
+freq.sp <- table(inventario$Especie)
+
+porciento <- freq.sp/sum(freq.sp)*100
+
+freq.ps <- table(inventario$Posicion)
+
+# Porcentaje de posición
+
+por.pos <- freq.ps/sum(freq.ps)*100
+
+barplot(freq.sp, col="red", las =1, border = NA)
+barplot(porciento, col="grey", las =1)
+
+barplot(freq.ps, las=1, col="lightblue", border=NA)
+
+
+pie(por.pos, col=topo.colors(4),
+    labels = paste(levels(inventario$Posicion), por.pos, "%"))
+
+
+pie(freq.sp, col = topo.colors(3),
+    labels = paste(levels(inventario$Especie), freq.sp, "ind"))
